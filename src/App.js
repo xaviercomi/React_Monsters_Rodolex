eb219d5cdf5b269b9c254d45187827c45b42bc96
xcomi <xaviercomi@gmail.com>
2023-08-10 10:32:45 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState
+        (
+          () => {
+            return { monsters: users };
+          }
+        )
+      )
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rodolex</h1>
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search-monsters'
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
